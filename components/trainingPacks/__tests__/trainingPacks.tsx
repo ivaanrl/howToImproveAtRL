36132bbe54cfc0498fdabca9fd0fcbd54a06981a
ivaanrl <ivanrl2010@gmail.com>
2020-10-29 21:48:32 -0300

    add tests

---
+++ components/trainingPacks/__tests__/trainingPacks.tsx
@@ -0,0 +1,49 @@
+import { render, waitFor } from '../../../test/testsUtils';
+import user from '@testing-library/user-event';
+import { TrainingPack } from '../../../shared/interfaces';
+import faker from 'faker';
+import TrainingPacks from '../trainingPacks';
+
+const trainingPacksInfo: TrainingPack[] = [
+  {
+    training_pack_id: faker.random.number(),
+    field_image: 'Mannfield',
+    difficulty: faker.lorem.word(),
+    training_pack_code: faker.random.alphaNumeric(16),
+    training_style: '{ "Striker": true, "Redirects": true, "Rebounds": true }',
+    training_pack_name: faker.lorem.words(5),
+    name: faker.lorem.words(2),
+    youtube_explanation: faker.internet.url(),
+  },
+  {
+    training_pack_id: faker.random.number(),
+    field_image: 'Mannfield',
+    difficulty: faker.lorem.word(),
+    training_pack_code: faker.random.alphaNumeric(16),
+    training_style: '{ "Saves": true, "Defensive": true}',
+    training_pack_name: faker.lorem.words(5),
+    name: faker.lorem.words(2),
+    youtube_explanation: faker.internet.url(),
+  },
+  {
+    training_pack_id: faker.random.number(),
+    field_image: 'Mannfield',
+    difficulty: faker.lorem.word(),
+    training_pack_code: faker.random.alphaNumeric(16),
+    training_style: '{ "Striker": true, "Passing": true, "Freestyle": true }',
+    training_pack_name: faker.lorem.words(5),
+    name: faker.lorem.words(2),
+    youtube_explanation: faker.internet.url(),
+  },
+];
+
+test('renders properly', () => {
+  const { getByRole } = render(
+    <TrainingPacks trainingPacksInfo={trainingPacksInfo} />,
+  );
+
+  const TrainigPacksContainer = getByRole('training-packs-container');
+
+  expect(TrainigPacksContainer).toBeInTheDocument();
+  expect(TrainigPacksContainer.children.length).toBe(trainingPacksInfo.length);
+});
--- components/trainingPacks/trainingPacks.tsx
+++ components/trainingPacks/trainingPacks.tsx
@@ -1,13 +1,13 @@
-import FeaturedTrainingPack from "../trainingPack/featuredTrainingPack";
-import { TrainingPack as TrainingPackInterface } from "../../shared/interfaces";
-import { TrainingPacksContainer } from "./trainingPacksStyle";
+import FeaturedTrainingPack from '../trainingPack/featuredTrainingPack';
+import { TrainingPack as TrainingPackInterface } from '../../shared/interfaces';
+import { TrainingPacksContainer } from './trainingPacksStyle';
 interface Props {
   trainingPacksInfo: TrainingPackInterface[];
 }
 
 const TrainingPacks = ({ trainingPacksInfo }: Props) => {
   return (
-    <TrainingPacksContainer>
+    <TrainingPacksContainer role="training-packs-container">
       {trainingPacksInfo?.map((trainingPack) => (
         <FeaturedTrainingPack
           trainingPackInfo={trainingPack}
